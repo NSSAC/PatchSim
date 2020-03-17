37f9f37dead8482ebd395aeb5e02bb4c9fc846ea
Parantapa Bhattacharya <pb@parantapa.net>
2020-03-17 13:00:34 -0400

    Add test_foi to pytest

--- test_patchsim/test_manual.py
+++ test_patchsim/test_manual.py
@@ -5,9 +5,17 @@ from util import assert_equal_files, chdir_context
 import patchsim as sim
 
 def test_det(workdir):
-    """Run the test_det script."""
+    """Run the test_det setting."""
     with chdir_context(workdir):
         configs = sim.read_config("cfg_test_det")
         sim.run_disease_simulation(configs, write_epi=True)
 
         assert_equal_files("test_det.out", "test_det.out.expected")
+
+def test_foi(workdir):
+    """Run the test_foi setting."""
+    with chdir_context(workdir):
+        configs = sim.read_config('cfg_test_foi')
+        sim.run_disease_simulation(configs, write_epi=True)
+
+        assert_equal_files("test_foi.out", "test_foi.out.expected")
