452f60b1c1562d0c002217101d91650b82a57f00
Parantapa Bhattacharya <pb@parantapa.net>
2020-03-17 13:12:51 -0400

    Add test_stoc to pytest

--- test_patchsim/test_manual.py
+++ test_patchsim/test_manual.py
@@ -4,6 +4,7 @@ from util import assert_equal_files, chdir_context
 
 import patchsim as sim
 
+
 def test_det(workdir):
     """Run the test_det setting."""
     with chdir_context(workdir):
@@ -12,18 +13,29 @@ def test_det(workdir):
 
         assert_equal_files("test_det.out", "test_det.out.expected")
 
+
 def test_foi(workdir):
     """Run the test_foi setting."""
     with chdir_context(workdir):
-        configs = sim.read_config('cfg_test_foi')
+        configs = sim.read_config("cfg_test_foi")
         sim.run_disease_simulation(configs, write_epi=True)
 
         assert_equal_files("test_foi.out", "test_foi.out.expected")
 
+
 def test_paramfile(workdir):
     """Run the test_paramfile setting."""
     with chdir_context(workdir):
-        configs = sim.read_config('cfg_paramfile')
+        configs = sim.read_config("cfg_paramfile")
         sim.run_disease_simulation(configs, write_epi=True)
 
         assert_equal_files("test.out", "test_paramfile.out.expected")
+
+
+def test_stoc(workdir):
+    """Run the test_stoc setting."""
+    with chdir_context(workdir):
+        configs = sim.read_config("cfg_test_stoc")
+        sim.run_disease_simulation(configs, write_epi=True)
+
+        assert_equal_files("test_stoc.out", "test_stoc.out.expected")
